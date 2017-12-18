e848aacba5281c7d638749af3cbc1b937fbadbf8
cranelv <cranelv@163.com>
2017-12-18 17:47:19 +0800

    add version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {};
-config.version = '0.1.5';
+config.version = '0.1.6';
 // web3 parameter
 config.host = '// http://localhost'; // http://localhost
 config.rpcIpcPath = process.env.HOME;
